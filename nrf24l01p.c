5539125d7139d4eebc4b7724f9c74bf3f4dea479
Tom Malone <tmalone+git@gmail.com>
2014-08-10 16:39:34 -0400

    Added comments/documentation at top of file.

---
+++ nrf24l01p.c
@@ -0,0 +1,11 @@
+/**
+ * nRF24L01+ library 
+ * File: nrf24l01p.c
+ * Author: Tom Malone
+ *
+ * Inspired by:
+ * 	Neil MacMillan:  http://nrqm.ca/nrf24l01/
+ * 	Brennan:         http://blog.diyembedded.com/
+ * 	Stefan Engelke:  http://tinkerer.eu/AVRLib/nRF24L01/
+ *
+*/
