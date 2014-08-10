b51b5e8a79a1c3dc4dfdbf2e4674f47ab008c5aa
Tom Malone <tmalone+git@gmail.com>
2014-08-10 17:09:07 -0400

    Added definitions for radio configuration (channel, address length) and mnemonic definitions for the pins on the microcontroller (Atmel AVR ATmega48/88/168/328).

--- nrf24l01p.c
+++ nrf24l01p.c
@@ -9,3 +9,22 @@
  * 	Stefan Engelke:  http://tinkerer.eu/AVRLib/nRF24L01/
  *
 */
+
+#include "nrf24l01p.h"
+
+// Non-Public Constants & Macros
+
+#define CHANNEL			112
+#define ADDRESS_LENGTH	5
+
+// Pin definitions for chip select and chip enable on the radio module
+#define CE_DDR			DDRB
+#define CE_PORT			PORTB
+#define CE_PIN			PB1
+#define CSN_DDR			DDRB
+#define CSN_PORT		PORTB
+#define CSN_PIN			PB2			// SS
+#define IRQ_DDR			DDRB
+#define IRQ_PORT		PORTB
+#define IRQ_PIN			PB0			// PCINT0
+
