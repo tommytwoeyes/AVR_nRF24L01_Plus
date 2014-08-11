241284b57ddec9c84fdb6a24590021998ecf7b34
Tom Malone <tmalone+git@gmail.com>
2014-08-10 20:59:02 -0400

    Added more realistic credit in comments.

--- nrf24l01p.c
+++ nrf24l01p.c
@@ -4,7 +4,7 @@
  * File: nrf24l01p.c
  * Author: Tom Malone
  *
- * Inspired by:
+ * Inspired by (and borrowed from):
  * 	Neil MacMillan:  http://nrqm.ca/nrf24l01/
  * 	Brennan:         http://blog.diyembedded.com/
  * 	Stefan Engelke:  http://tinkerer.eu/AVRLib/nRF24L01/
